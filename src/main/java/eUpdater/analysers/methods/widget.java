feeeb0848c4d84bff35cf56855765f5d9d2baf4b
elfyyy <kyleh04@ymail.com>
2017-02-24 14:38:59 -0500

    Fixes broken widget hooks

--- src/main/java/eUpdater/analysers/methods/widget.java
+++ src/main/java/eUpdater/analysers/methods/widget.java
@@ -24,7 +24,7 @@ public class widget extends methodAnalyserFrame {
         this.setParent("Widget", classes.myWidget);
         this.setNeededHooks(Arrays.asList("Children", "ItemID", "ItemAmount", "WidgetID", "Name", "Text", "IsHidden",
                 "AbsoluteY", "AbsoluteX", "RelativeX", "RelativeY", "Width", "Height", "ParentID", "ScrollY", "ScrollX",
-                "InvIDs", "BoundsIndex", "StackSizes"));
+                "InvIDs", "BoundsIndex", "StackSizes", "TextureID", "Parent"));
 
         addHook(new hook("Children", parentClass.getFields("[L" + classes.myWidget.getName() + ";").get(0)));
 
@@ -32,79 +32,68 @@ public class widget extends methodAnalyserFrame {
         for (classFrame c : CLASSES.values()) {
             List<MethodNode> methodList = c.methods;
             for (MethodNode m : methodList) {
-                int L = 0;
-                AbstractInsnNode[] Instructions = m.instructions.toArray();
-                Searcher search = new Searcher(m);
-                for (int I = 0; L != -1; ++I) {
-                    L = search.find(new int[]{Opcodes.SIPUSH}, I);
-                    if (L != -1)
-                        if ((((IntInsnNode) Instructions[L]).operand == 2100))
-                            method = m;
-                }
+                if (c.name.equals(classes.myWidget.getName()) && m.name.equals("<init>"))
+                    method = m;
             }
         }
 
         AbstractInsnNode[] Instructions = method.instructions.toArray();
+        int L;
         Searcher search = new Searcher(method);
-        int L = search.findSingleIntValue(Opcodes.SIPUSH, 1701);/*
-        L = search.find(new int[]{Opcodes.GETFIELD}, 0, L);
-        addHook(new hook("ItemID", Instructions, L));
-
-        L = search.findSingleJump(Opcodes.GOTO, Opcodes.GETFIELD, L + 5, 15, 0);
-        addHook(new hook("ItemAmount", Instructions, L));
-
-        L = search.findSingleIntValue(Opcodes.SIPUSH, 1121);
-        L = search.findSingleJump(Opcodes.GOTO, Opcodes.GETFIELD, L, 10, 0);
+        L = search.findSingleJump(Opcodes.GOTO, Opcodes.PUTFIELD, 0, 10, 1);
         addHook(new hook("WidgetID", Instructions, L));
 
-        L = search.findSingleIntValue(Opcodes.SIPUSH, 1305);
-        L = search.findSingleJump(Opcodes.GOTO, Opcodes.PUTFIELD, L, 10, 0);
-        addHook(new hook("Name", Instructions, L));
-
-        L = search.findSingleIntValue(Opcodes.SIPUSH, 2602);
-        L = search.findSingleJump(Opcodes.GOTO, Opcodes.GETFIELD, L, 15, 0);
-        addHook(new hook("Text", Instructions, L));
-
-        L = search.findSingleIntValue(Opcodes.SIPUSH, 2504);
-        L = search.findSingleJump(Opcodes.GOTO, Opcodes.GETFIELD, L, 20, 0);
-        addHook(new hook("IsHidden", Instructions, L));
-
-        L = search.findSingleIntValue(Opcodes.SIPUSH, 1501);
-        L = search.findSingleJump(Opcodes.GOTO, Opcodes.GETFIELD, L, 10, 0);
-        addHook(new hook("AbsoluteY", Instructions, L));
-
-        L = search.findSingleIntValue(Opcodes.SIPUSH, 1500, L - 100);
-        L = search.findSingleJump(Opcodes.GOTO, Opcodes.GETFIELD, L, 10, 0);
+        L = search.findSingleJump(Opcodes.GOTO, Opcodes.PUTFIELD, L + 1, 50, 11);
         addHook(new hook("AbsoluteX", Instructions, L));
 
-        L = search.findSingleIntValue(Opcodes.SIPUSH, 1601);
-        L = search.findSingleJump(Opcodes.GOTO, Opcodes.GETFIELD, L, 10, 0);
-        addHook(new hook("RelativeY", Instructions, L));
+        L = search.findSingleJump(Opcodes.GOTO, Opcodes.PUTFIELD, L + 1, 50, 0);
+        addHook(new hook("AbsoluteY", Instructions, L));
 
-        L = search.findSingleIntValue(Opcodes.SIPUSH, 1600, L - 100);
-        L = search.findSingleJump(Opcodes.GOTO, Opcodes.GETFIELD, L, 20, 0);
-        addHook(new hook("RelativeX", Instructions, L));
-
-        L = search.findSingleIntValue(Opcodes.SIPUSH, 2502);
-        L = search.findSingleJump(Opcodes.GOTO, Opcodes.GETFIELD, L, 20, 0);
+        L = search.findSingleJump(Opcodes.GOTO, Opcodes.PUTFIELD, L + 1, 50, 0);
         addHook(new hook("Width", Instructions, L));
 
-        L = search.findSingleIntValue(Opcodes.SIPUSH, 2503);
-        L = search.findSingleJump(Opcodes.GOTO, Opcodes.GETFIELD, L, 10, 0);
+        L = search.findSingleJump(Opcodes.GOTO, Opcodes.PUTFIELD, L + 1, 50, 0);
         addHook(new hook("Height", Instructions, L));
 
-        L = search.findSingleIntValue(Opcodes.SIPUSH, 2505);
-        L = search.findSingleJump(Opcodes.GOTO, Opcodes.GETFIELD, L, 20, 0);
+        L = search.findSingleJump(Opcodes.GOTO, Opcodes.PUTFIELD, L + 1, 50, 2);
         addHook(new hook("ParentID", Instructions, L));
 
-        L = search.findSingleIntValue(Opcodes.SIPUSH, 1601);
-        L = search.findSingleJump(Opcodes.GOTO, Opcodes.GETFIELD, L, 10, 0);
+        L = search.findSingleJump(Opcodes.GOTO, Opcodes.PUTFIELD, L + 1, 50, 0);
+        addHook(new hook("IsHidden", Instructions, L));
+
+        L = search.findSingleJump(Opcodes.GOTO, Opcodes.PUTFIELD, L + 1, 50, 0);
+        addHook(new hook("RelativeX", Instructions, L));
+
+        L = search.findSingleJump(Opcodes.GOTO, Opcodes.PUTFIELD, L + 1, 50, 0);
+        addHook(new hook("RelativeY", Instructions, L));
+
+        L = search.findSingleJump(Opcodes.GOTO, Opcodes.PUTFIELD, L + 1, 50, 12);
+        addHook(new hook("TextureID", Instructions, L));
+
+        L = search.findSingleJump(Opcodes.GOTO, Opcodes.PUTFIELD, L + 1, 150, 22);
+        addHook(new hook("Text", Instructions, L));
+
+        L = search.findSingleJump(Opcodes.GOTO, Opcodes.PUTFIELD, L + 1, 50, 8);
+        addHook(new hook("Name", Instructions, L));
+
+        L = search.findSingleJump(Opcodes.GOTO, Opcodes.PUTFIELD, L + 1, 50, 0);
+        addHook(new hook("Parent", Instructions, L));
+
+        L = search.findSingleJump(Opcodes.GOTO, Opcodes.PUTFIELD, L + 1, 50, 8);
+        addHook(new hook("ItemID", Instructions, L));
+
+        L = search.findSingleJump(Opcodes.GOTO, Opcodes.PUTFIELD, L + 1, 50, 0);
+        addHook(new hook("ItemAmount", Instructions, L));
+
+        L = search.findSingleJump(Opcodes.GOTO, Opcodes.PUTFIELD, L + 1, 50, 8);
+        addHook(new hook("BoundsIndex", Instructions, L));
+
+        L = search.findSingleJump(Opcodes.GOTO, Opcodes.PUTFIELD, 0, 200, 21);
+        addHook(new hook("ScrollX", Instructions, L));
+
+        L = search.findSingleJump(Opcodes.GOTO, Opcodes.PUTFIELD, 0, 200, 22);
         addHook(new hook("ScrollY", Instructions, L));
 
-        L = search.findSingleIntValue(Opcodes.SIPUSH, 1600, L - 50);
-        L = search.findSingleJump(Opcodes.GOTO, Opcodes.GETFIELD, L, 20, 0);
-        addHook(new hook("ScrollX", Instructions, L));
-*/
         for (classFrame c : CLASSES.values()) {
             method = c.getMethod(true, "([L" + classes.myWidget.getName() + ";IIIIIIII)V");
             if (method != null) {
